44f4daa0a7ddb2cb99bec827dd668dcc486c4ad6
Vitor Hugo <vitorhugo2005_br@yahoo.com.br>
2022-12-11 01:19:02 -0300

    Create Personal_Heath.py

---
+++ Personal_Heath.py
@@ -0,0 +1,8 @@
+
+#A ideia inicial é criar um programa voltado para auxilio na saúde.
+
+
+print("Olá caro usuário , esse programa tem como missão principal te auxiliar")
+nome = str(input("Informe seu nome completo"))
+idade = int(input("Informe sua idade:"))
+peso = float(input("Informe seu peso:"))
